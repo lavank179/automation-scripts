33a4e4d6718576dc0ed6ae15b588fc4b2c0fed8a
lavank179 <chlavankumar179@gmail.com>
2025-05-30 10:43:19 +0530

    separate test file for shellcheck

---
+++ tests/linux-shellcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set +e
+
+cd linux-unix/
+FILES=(*)
+errs=0
+for FILE in "${FILES[@]}"; do
+  echo "Checking $FILE ..."
+  shellcheck $FILE
+  er=$?
+  if [[ $er -ne 0 ]]; then
+    errs=$(($errs+1))
+  fi
+  echo "-----------------------------------------------------------------------------------------------------------------------"
+done
+if [[ $errs -gt 0 ]]; then
+  echo "FAILED: $errs Erros in shell script files. Kindly check above"
+  exit 1
+else
+  echo "🎉 Woot! No lint or syntax errors."
+fi
